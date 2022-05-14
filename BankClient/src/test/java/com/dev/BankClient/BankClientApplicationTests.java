f8be22cb8a77d0e7df3767cdb822e7bc88cb19d7
pooyafils <pooyafils@gmail.com>
2022-05-14 21:38:09 +0430

    project init

---
+++ BankClient/src/test/java/com/dev/BankClient/BankClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.BankClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
