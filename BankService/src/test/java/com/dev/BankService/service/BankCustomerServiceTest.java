0e02f75947e29265164d465608052cf48025dbaa
pooyafils <pooyafils@gmail.com>
2022-05-19 03:06:25 +0430

    developing integration test for services

--- BankService/src/test/java/com/dev/BankService/service/BankCustomerServiceTest.java
+++ BankService/src/test/java/com/dev/BankService/service/BankCustomerServiceTest.java
@@ -1,21 +1,57 @@
 package com.dev.BankService.service;
 
+import com.dev.BankService.model.Currency;
+import com.dev.BankService.repository.CurrencyRepository;
+import com.dev.BankService.repository.TransactionRepository;
+import org.hibernate.annotations.Source;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DirtiesContext
+@SpringBootTest
 class BankCustomerServiceTest {
+    @Autowired
+    TransactionRepository transactionRepository;
+    @Autowired
+    CurrencyRepository currencyRepository;
+@Autowired
+        BankCustomerService bankCustomerService;
+    Currency currency;
+    String accountId="29bd810a-d6f0-11ec-9d64-0242ac120002";
+    String customerId="80289b74-d6f0-11ec-9d64-0242ac120002";
 
     @BeforeEach
     void setUp() {
+        currency=new Currency();
+        currency.setAccountId(UUID.fromString(accountId));
+        currency.setName("EUR");
+        currency.setBalance(0);
+        currency.setId(1);
+        currency.setCustomerId(UUID.fromString(customerId));
+        currencyRepository.save(currency);
     }
 
     @Test
     void createCustomer() {
     }
 
-    @Test
-    void getCustomerAccount() {
+    @ParameterizedTest()
+    @ValueSource(strings = "29bd810a-d6f0-11ec-9d64-0242ac120002")
+    public void getCustomerAccount(String accountId) {
+        Currency currency= new Currency();
+        currency=currencyRepository.findByAccountId(UUID.fromString(accountId));
+        assertEquals(0,bankCustomerService.getCustomerAccount(accountId).getBalance());
+        assertNotNull(bankCustomerService.getCustomerAccount(accountId));
+
     }
 }
--- BankService/src/test/java/com/dev/BankService/service/TransactionServiceTest.java
+++ BankService/src/test/java/com/dev/BankService/service/TransactionServiceTest.java
@@ -1,43 +1,72 @@
 package com.dev.BankService.service;
 
 import com.dev.BankService.exception.CustomExceptions;
+import com.dev.BankService.model.Currency;
 import com.dev.BankService.model.Transaction;
+import com.dev.BankService.repository.CurrencyRepository;
 import com.dev.BankService.repository.TransactionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.Currency;
 import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.verify;
 @SpringBootTest
+
 class TransactionServiceTest {
 @Autowired
     TransactionRepository transactionRepository;
 @Autowired
         TransactionService transactionService;
+@Autowired
+    CurrencyRepository currencyRepository;
 Transaction transaction;
    String accountId="9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
     String accountIdTest="9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
-
+    String transactionIdTest="8d569b00-d6e3-11ec-9d64-0242ac120002";
+    Currency currency;
     UUID  transactionId=UUID.randomUUID();
+
+
+
     @BeforeEach
     void setUp() {
         transaction=new Transaction(1, UUID.fromString(accountId),transactionId,12,"IN","adding",12);
         transactionRepository.save(transaction);
+         currency=new Currency();
+        currency.setId(1);
+        currency.setName("EUR");
+        currency.setBalance(0);
+        currency.setAccountId(UUID.fromString(accountIdTest));
+        currencyRepository.save(currency);
     }
 
     @Test
     void postTransaction() {
+        Transaction postObj=new Transaction(2, UUID.fromString(accountIdTest),UUID.fromString(transactionIdTest),120,"IN","adding",120);
+        Currency currencyAccount=currencyRepository.findByAccountIdAndName(UUID.fromString(accountId),"EUR");
+        assertNotNull(currencyAccount);
+     Transaction transaction=  transactionService.postTransaction(accountId,140,"EUR","IN","ADDING TO THE ACCOUNT");
+     assertEquals(transaction.getAmount(),140);
     }
 
     @Test
     void getTransactions() {
         List<Transaction> transactions=transactionService.getTransactions(accountId);
-        assertEquals(1,transactionService.getTransactions(accountId).size());
+        assertNotNull(transactions);
+
     }
 }
